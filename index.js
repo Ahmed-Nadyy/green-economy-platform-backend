bc3a087be469e155eab63ef05fabb27d019963e4
SABRY225 <ahmed010144497@gmail.com>
2025-06-04 03:30:55 +0300

    edit in file index

--- index.js
+++ index.js
@@ -21,31 +21,29 @@ app.get("/", (req, res) => {
 
 // Auth routes
 const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 // Admin management routes
 const adminRoutes = require("./routes/adminRoutes");
-app.use("/api/admins", adminRoutes);
+app.use("/api/v1/admins", adminRoutes);
 
 // Job request routes
 const jobRequestRoutes = require("./routes/jobRequestRoutes");
-app.use("/api/job-requests", jobRequestRoutes);
+app.use("/api/v1/job-requests", jobRequestRoutes);
 
 // Gallery routes
 const galleryRoutes = require("./routes/galleryRoutes");
-app.use("/api/gallery", galleryRoutes);
+app.use("/api/v1/gallery", galleryRoutes);
 
 // Crop routes
 const cropRoutes = require("./routes/cropRoutes");
-app.use("/api/crops", cropRoutes);
+app.use("/api/v1/crops", cropRoutes);
 
 // Article routes
 const articleRoutes = require("./routes/articleRoutes");
-app.use("/api/articles", articleRoutes);
+app.use("/api/v1/articles", articleRoutes);
 
-// Example: user routes
-// const userRoutes = require('./routes/user.routes');
-// app.use('/api/users', userRoutes);
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
