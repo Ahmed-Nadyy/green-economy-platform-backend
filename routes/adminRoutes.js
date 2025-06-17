6c2fdbae455b6b7bc679576fe58c2c411665ebdc
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-06-17 17:58:14 +0300

    remove images and videos limits

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -14,9 +14,6 @@ const {
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
-  },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
